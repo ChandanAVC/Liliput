f189f340c94a29e67d049d48a1aa59bb739b029f
LiliputVtuForum <rajathakr123@gmail.com>
2015-09-30 01:16:46 +0530

    Create captcha.php

---
+++ captcha.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+	session_start();
+	include("./phptextClass.php");	
+	
+	/*create class object*/
+	$phptextObj = new phptextClass();	
+	/*phptext function to genrate image with text*/
+	$phptextObj->phpcaptcha('#162453','#fff',120,40,10,25);	
+ ?>
