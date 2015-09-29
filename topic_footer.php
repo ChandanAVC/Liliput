1a07a76cbbd842aca2072f0de9895205ddb5c01a
LiliputVtuForum <rajathakr123@gmail.com>
2015-09-30 01:55:11 +0530

    Create topic_footer.php

---
+++ topic_footer.php
@@ -0,0 +1,49 @@
+<div class="col-sm-3">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2>Side</h2>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Gadget Guru</div>
+                                <div class="panel-body">
+                                    Microsoft's HoloLens headset will reach developers within a year..
+                                </div>
+                                <div class="text-center">
+                                    <a href="http://www.engadget.com/2015/08/01/microsoft-hololens-time-frame"><i class="fa fa-plus"></i>read more..</a>
+                                </div>
+                            </div>
+                            <hr />
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Tech News</div>
+                                <div class="panel-body">
+                                  Stephen Hawking Kicks Off $100M Hunt for Alien Intelligence  
+                                </div>
+                                <div class="text-center">
+                                    <a href="http://www.technewsworld.com/story/Stephen-Hawking-Kicks-Off-100M-Hunt-for-Alien-Intelligence-82292.html"><i class="fa fa-plus"></i>Know more...</a>
+                                </div>
+                            </div>
+                            <hr />
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Quote for the day</div>
+                                <div class="panel-body">
+                                    You have to dream before your dreams can come true
+                                </div>
+                                <div class="text-center">
+                                    <a href="#"><i class="fa fa-plus"></i>for more Quotes</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="topic/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+    <!-- Page Script -->
+    <script>
+      $(function () {
+        //Add text editor
+        $("#compose-textarea").wysihtml5();
+      });
+    </script>
+</body>
+</html>
