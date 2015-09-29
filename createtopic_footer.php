f3bc606e1276e4591ffbc7169625fdbb5023de3d
LiliputVtuForum <rajathakr123@gmail.com>
2015-09-30 01:29:52 +0530

    Create createtopic_footer.php

---
+++ createtopic_footer.php
@@ -0,0 +1,41 @@
+<script src="assests/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+    <!-- Page Script -->
+    <script>
+      $(function () {
+        //Add text editor
+        $("#compose-textarea").wysihtml5();
+      });
+    </script>
+ </div><!-- djkndks-->
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 social-login">
+                        	<h3 style="font-family: 'Dancing Script', cursive;"></h3>
+                        	<div class="social-login-buttons">
+	                        	<a class="btn btn-link-2" href="forum_main.php">
+	                        		<i class="fa fa-arrow-circle-left"></i> Back to Forum
+	                        	</a>
+	                        
+	   
+                        	</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+
+    </body>
+
+</html>
