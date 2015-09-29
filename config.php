0dffe45d66299847a78871a5823c4a1cdf5620a6
LiliputVtuForum <rajathakr123@gmail.com>
2015-09-30 01:21:17 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'JD7OpWmlSGgJfRNlZh6Ybwbig');
+define('CONSUMER_SECRET', 'BmeTh0GnK2kYYmVhI16sYMCDDYDv3PXGsdnxXGzMOU7JBxAToY');
+define('OAUTH_CALLBACK', 'http://localhost/Liliput/process.php');
+?>
