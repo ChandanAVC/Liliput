23f36c35836a4a5de995b65a5b1b432a0c3a4f0f
LiliputVtuForum <rajathakr123@gmail.com>
2015-09-30 01:31:07 +0530

    Create fbconfig.php

---
+++ fbconfig.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+require_once("autoload.php");
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+// init app with app id and secret
+FacebookSession::setDefaultApplication( '1635216683393110','f95305821d9de21a76c5d346f445e919' );
+// login helper with redirect_uri
+    $helper = new FacebookRedirectLoginHelper('http://localhost/talentspear/forum_main.php' );
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+}
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+     	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
+ 	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
+	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
+	/* ---- Session Variables -----*/
+	    $_SESSION['FBID'] = $fbid;           
+        $_SESSION['FULLNAME'] = $fbfullname;
+	    $_SESSION['EMAIL'] =  $femail;
+    /* ---- header location after session ----*/
+  header("Location: index.php");
+} else {
+  $loginUrl = $helper->getLoginUrl();
+ header("Location: ".$loginUrl);
+}
+?>
